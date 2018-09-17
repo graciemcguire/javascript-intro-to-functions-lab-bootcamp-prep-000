587cc2972773d0377f30bd89a6db459550a6c1ea
Gracie McGuire <m.graciemcguire@gmail.com>
2018-09-17 22:37:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function shout(string){
   return string.toUpperCase();
 }
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+logShout(string){
+  console.log(string.toUpperCase());
+}
+
+logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+sayHiToGrandma(string){
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  }
+}
